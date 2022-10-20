fbe216de6fc9a96ba28a96927f18f633e526d1af
Daniel Weiß <daniel.weiss@grollmus.de>
2022-10-20 17:15:20 +0200

    test hook

--- terraform-basic-structure/1_terraform.tf
+++ terraform-basic-structure/1_terraform.tf
@@ -1,8 +1,7 @@
 # You can save your terraform state file on a remote, recommended for projects with more than one developer
 
 terraform {
-  backend "remote" {
-    organization = "example_corp"
+  backend "remote" {organization = "example_corp"
 
     workspaces {
       name = "my-app-prod"
