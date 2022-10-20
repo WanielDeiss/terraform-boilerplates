08f0712d99fd0cf9692aad56f3f4e6f1c0869521
Daniel Weiß <daniel.weiss@grollmus.de>
2022-10-20 17:49:08 +0200

    add terraform version

--- terraform-basic-structure/1_terraform.tf
+++ terraform-basic-structure/1_terraform.tf
@@ -1,6 +1,8 @@
 # You can save your terraform state file on a remote, recommended for projects with more than one developer
 
 terraform {
+  required_version = ">= 1.3.3"
+
   backend "remote" {
     organization = "example_corp"
 
