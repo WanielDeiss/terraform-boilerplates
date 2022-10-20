a2d2a494f53d9c1f70d8c64e11b7a58b90aaac38
Daniel Weiß <daniel.weiss@grollmus.de>
2022-10-20 17:14:22 +0200

    file structure

---
+++ terraform-basic-structure/1_terraform.tf
@@ -0,0 +1,11 @@
+# You can save your terraform state file on a remote, recommended for projects with more than one developer
+
+terraform {
+  backend "remote" {
+    organization = "example_corp"
+
+    workspaces {
+      name = "my-app-prod"
+    }
+  }
+}
